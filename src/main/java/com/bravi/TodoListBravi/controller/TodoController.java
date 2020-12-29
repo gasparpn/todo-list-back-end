6ae12b279b8c88472bd000b14a98b2753f4ce51a
Gaspar Neto <gasparneto95@gmail.com>
2020-12-29 19:04:35 -0300

    Configure Cors for the controller
    - Add configuration to permit cross origin request. Necessary for the front-end acess this project.

--- src/main/java/com/bravi/TodoListBravi/controller/TodoController.java
+++ src/main/java/com/bravi/TodoListBravi/controller/TodoController.java
@@ -17,21 +17,25 @@ public class TodoController {
     @Autowired
     private ITodoService todoService;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping
     public List<TodoItem> findAll(){
         return todoService.findAll();
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping
     public TodoItem save(@Valid @NotNull @RequestBody TodoItem todoItem){
         return todoService.save(todoItem);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PutMapping
     public TodoItem update(@Valid @NotNull @RequestBody TodoItem todoItem){
         return todoService.save(todoItem);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @DeleteMapping(value = "/{id}")
     public void delete(@PathVariable Long id){
         todoService.delete(id);
